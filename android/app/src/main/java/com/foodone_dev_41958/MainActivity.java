75197aeba30abb8aa8c4c81a1a522a047c74d33c
Crowdbotics <engineering@crowdbotics.com>
2022-11-01 05:49:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/foodone_dev_41958/MainActivity.java
@@ -0,0 +1,15 @@
+package com.foodone_dev_41958;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "foodone_dev_41958";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "foodone_dev_41958",
+  url: "https://foodone_dev_41958.botics.co",
+  api: "https://foodone_dev_41958.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
